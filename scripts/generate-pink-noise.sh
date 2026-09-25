706cea0d817d2924a6e131d798d03dbc0b82def9
Charles Buckman <buckmanc@gmail.com>
2026-09-25 00:35:09 -0500

    numerous tweaks

--- scripts/generate-pink-noise.sh
+++ scripts/generate-pink-noise.sh
@@ -13,17 +13,31 @@ fi
 output_file="$1"
 duration="$2"
 
+outputDir="$(dirname "$output_file")"
+outputFileNameFull="$(basename "$output_file")"
+# outputFileName="${outputFileNameFull%.*}"
+# outputExt="${outputFileNameFull##*.}"
+
 # Create temp file in same directory as output for atomic move
-temp_file="$(dirname "$output_file")/.pink_noise_$(basename "$output_file").tmp"
+# tempTempDir="${outputDir}/tmp/"
+# mkdir -p "$tempTempDir"
 
 # Generate stereo pink noise
+# generates a ~13 GB file for 15 hours of pink noise
+# gotta leave --guardrails out as this causes double the required storage space
+# don't need --temp as we're avoiding triggering temp dir usage due to storage limits
 sox --combine merge \
-    "|sox -n -r 44100 -c 1 -p --guard synth ${duration} pinknoise vol 0.9" \
-    "|sox -n -r 44100 -c 1 -p --guard synth ${duration} pinknoise vol 0.9" \
-    -c 2 -r 44100 "$temp_file"
+    "|sox -n -r 44100 -c 1 -p synth ${duration} pinknoise vol 0.9" \
+    "|sox -n -r 44100 -c 1 -p synth ${duration} pinknoise vol 0.9" \
+    -c 2 -r 44100 "$output_file"
 
-# Atomic move to final destination
-mv "$temp_file" "$output_file"
+# mono version
+# sox -n -r 44100 -c 1 -p synth ${duration} pinknoise vol 0.9 "$output_file"
+
+(cd "$outputDir" && ls -sh "$outputFileNameFull")
 
 # ideally this shouldn't be here, but if it's called from the main script it'll re-prompt for sudo auth after the long sox gen call
-chown mpd:audio "$output_file"
+if id "mpd" >/dev/null 2>&1
+then
+    chown mpd:audio "$output_file"
+fi
